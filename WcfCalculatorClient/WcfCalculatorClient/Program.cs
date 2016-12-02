cd1982814a918b4d8ee8b2e25b007ec3dd268e16
Anastasia Paramonova <Anastasia Paramonova>
2016-12-02 18:10:56 +0300

    add service contract

---
+++ WcfCalculatorClient/WcfCalculatorClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WcfCalculatorClient.CalculatorServiceReference;
+
+namespace WcfCalculatorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorClient client = new CalculatorClient();
+            Console.WriteLine(client.Sqrt(45));
+            client.Close();
+            Console.ReadLine();
+        }
+    }
+}
